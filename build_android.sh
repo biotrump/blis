f82e921a93c559db5ef29e8527d30c6d82135501
thomas tsai <thomas@biotrump.com>
2015-04-24 11:37:27 +0800

    NDK r10d ANDROID_APIVER=android-14 TOOL_VER="4.9"

--- build_android.sh
+++ build_android.sh
@@ -1,34 +1,10 @@
 #!/bin/bash
 # Compiles ffts for Android
 # Make sure you have NDK_ROOT defined in .bashrc or .bash_profile
-# Modify INSTALL_DIR to suit your situation
-#Lollipop	5.0 - 5.1	API level 21, 22
-#KitKat	4.4 - 4.4.4	API level 19
-#Jelly Bean	4.3.x	API level 18
-#Jelly Bean	4.2.x	API level 17
-#Jelly Bean	4.1.x	API level 16
-#Ice Cream Sandwich	4.0.3 - 4.0.4	API level 15, NDK 8
-#Ice Cream Sandwich	4.0.1 - 4.0.2	API level 14, NDK 7
-#Honeycomb	3.2.x	API level 13
-#Honeycomb	3.1	API level 12, NDK 6
-#Honeycomb	3.0	API level 11
-#Gingerbread	2.3.3 - 2.3.7	API level 10
-#Gingerbread	2.3 - 2.3.2	API level 9, NDK 5
-#Froyo	2.2.x	API level 8, NDK 4
-NDK_ROOT=${NDK_ROOT:-/home/thomas/aosp/NDK/android-ndk-r10d}
-export NDK_ROOT
-#gofortran is supported in r9
-#export NDK_ROOT=/home/thomas/aosp/NDK/android-ndk-r9
+#export CMAKE_BUILD_TYPE "Debug"
+export CMAKE_BUILD_TYPE="Release"
 
-INSTALL_DIR="`pwd`/java/android/bin"
-
-#ANDROID_APIVER=android-8
-#ANDROID_APIVER=android-9
-#android 4.0.1 ICS and above
-ANDROID_APIVER=android-14
-#TOOL="4.6"
-TOOL="4.8"
-
+#get cpu counts
 case $(uname -s) in
   Darwin)
     CONFBUILD=i386-apple-darwin`uname -r`
@@ -46,49 +22,89 @@ CYGWIN*)
   *) echo $0: Unknown platform; exit
 esac
 
+INSTALL_DIR="`pwd`/java/android/bin"
+
+# Modify INSTALL_DIR to suit your situation
+#Lollipop	5.0 - 5.1	API level 21, 22
+#KitKat	4.4 - 4.4.4	API level 19
+#Jelly Bean	4.3.x	API level 18
+#Jelly Bean	4.2.x	API level 17
+#Jelly Bean	4.1.x	API level 16
+#Ice Cream Sandwich	4.0.3 - 4.0.4	API level 15, NDK 8
+#Ice Cream Sandwich	4.0.1 - 4.0.2	API level 14, NDK 7
+#Honeycomb	3.2.x	API level 13
+#Honeycomb	3.1	API level 12, NDK 6
+#Honeycomb	3.0	API level 11
+#Gingerbread	2.3.3 - 2.3.7	API level 10
+#Gingerbread	2.3 - 2.3.2	API level 9, NDK 5
+#Froyo	2.2.x	API level 8, NDK 4
+
+export NDK_ROOT=${HOME}/NDK/android-ndk-r10d
+#gofortran is supported in r9
+#export NDK_ROOT=${HOME}/NDK/android-ndk-r9
+export ANDROID_NDK=${NDK_ROOT}
+
+if [[ ${NDK_ROOT} =~ .*"-r9".* ]]
+then
+#ANDROID_APIVER=android-8
+#ANDROID_APIVER=android-9
+#android 4.0.1 ICS and above
+ANDROID_APIVER=android-14
+#TOOL_VER="4.6"
+#gfortran is in r9d V4.8.0
+TOOL_VER="4.8.0"
+else
+#android 4.0.1 ICS and above
+ANDROID_APIVER=android-14
+TOOL_VER="4.9"
+fi
+
+#default is arm
 arm=${arm:-arm}
 echo arm=$arm
 case arm in
   arm)
     TARGPLAT=arm-linux-androideabi
-    ARCHI=arm
     CONFTARG=arm-eabi
+    ARCHI=arm
   ;;
   x86)
-    TARGPLAT=x86
-    ARCHI=x86
+    TARGPLAT=i686-linux-android
     CONFTARG=x86
+    ARCHI=x86
   ;;
   mips)
   ## probably wrong
     TARGPLAT=mipsel-linux-android
-    ARCHI=mips
     CONFTARG=mips
+    ARCHI=mips
   ;;
   *) echo $0: Unknown target; exit
 esac
+echo ARCHI=$ARCHI
+#: ${NDK_ROOT:?}
 
-: ${NDK_ROOT:?}
-
-echo "Using: $NDK_ROOT/toolchains/${TARGPLAT}-${TOOL}/prebuilt/${HOSTPLAT}/bin"
+echo "Using: $NDK_ROOT/toolchains/${TARGPLAT}-${TOOL_VER}/prebuilt/${HOSTPLAT}/bin"
 export ARCHI
-#export PATH="$NDK_ROOT/toolchains/${TARGPLAT}-${TOOL}/prebuilt/${HOSTPLAT}/bin/:\
-#$NDK_ROOT/toolchains/${TARGPLAT}-${TOOL}/prebuilt/${HOSTPLAT}/${TARGPLAT}/bin/:$PATH"
-export PATH="$NDK_ROOT/toolchains/${TARGPLAT}-${TOOL}/prebuilt/${HOSTPLAT}/bin/:$PATH"
-#gcc --version
-#exit
-export SYS_ROOT="$NDK_ROOT/platforms/${ANDROID_APIVER}/arch-${ARCHI}/"
+#export PATH="$NDK_ROOT/toolchains/${TARGPLAT}-${TOOL_VER}/prebuilt/${HOSTPLAT}/bin/:\
+#$NDK_ROOT/toolchains/${TARGPLAT}-${TOOL_VER}/prebuilt/${HOSTPLAT}/${TARGPLAT}/bin/:$PATH"
+export PATH="${NDK_ROOT}/toolchains/${TARGPLAT}-${TOOL_VER}/prebuilt/${HOSTPLAT}/bin/:$PATH"
+echo $PATH
+export SYS_ROOT="${NDK_ROOT}/platforms/${ANDROID_APIVER}/arch-${ARCHI}/"
 export CC="${TARGPLAT}-gcc --sysroot=$SYS_ROOT"
 export LD="${TARGPLAT}-ld"
 export AR="${TARGPLAT}-ar"
+export ARCH=${AR}
 export RANLIB="${TARGPLAT}-ranlib"
 export STRIP="${TARGPLAT}-strip"
 #export CFLAGS="-Os -fPIE"
 export CFLAGS="-Os -fPIE --sysroot=$SYS_ROOT"
 export CXXFLAGS="-fPIE --sysroot=$SYS_ROOT"
+export FORTRAN="${TARGPLAT}-gfortran --sysroot=$SYS_ROOT"
 
-#include path :
-#platforms/android-21/arch-arm/usr/include/
+#!!! quite importnat for cmake to define the NDK's fortran compiler.!!!
+#Don't let cmake decide it.
+export FC=${FORTRAN}
 
 make clean
 
